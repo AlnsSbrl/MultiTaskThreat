ec1b482f310f202b218eb75c9d6e8c6db5b96d4f
alnssbrl <alonsosobralpablo@gmail.com>
2022-10-10 15:06:09 +0200

    engadindo as carreiras de fios

---
+++ ThreadRace/ThreadRace/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ThreadRace
+{
+    internal class Program
+    {
+        static readonly private object candado = new object();
+        static void Main(string[] args)
+        {
+            int suma = 0;
+
+
+            bool parar = false;
+            //dos hilos que compiten entre si, sumandose a si mismos, restando al otro
+            Thread thread1 = new Thread(() =>
+            {
+                while (!parar)
+                {
+                    lock (candado)
+                    {
+
+                        if (!parar)
+                        {
+
+                            ++suma;
+                            Console.WriteLine($"\tSuma: {suma}");
+                        }
+                        if (suma >= 1000)
+                        {
+                            parar = true;
+                        }
+                    }
+                }
+            });
+            Thread thread2 = new Thread(() =>
+            {
+                while (!parar)
+                {
+                    lock (candado)
+                    {
+
+                        if (!parar)
+                        {
+
+                            --suma;
+                            Console.WriteLine($"\tSuma: {suma}");
+                        }
+                        if (suma <= -1000) { parar = true; }
+                    }
+                }
+            });
+            thread1.Start();
+            thread2.Start();
+            Console.ReadKey();
+        }
+    }
+}
